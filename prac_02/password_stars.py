d164835da3a6eb1e32854ee540b137fd622e1dd3
Tim Osmond <timothy.osmond@my.jcu.edu.au>
2022-09-23 15:40:44 +1000

    Refactor password check program to use functions

--- prac_02/password_stars.py
+++ prac_02/password_stars.py
@@ -7,12 +7,23 @@ PASSWORD_LENGTH = 6
 
 
 def main():
+    """Get password and check length. Print asterisks for each character in password."""
+    password = get_password()
+    print_asterisks(password)
+
+
+def print_asterisks(password):
+    """Print asterisks for each character in password."""
+    print('*' * len(password))
+
+
+def get_password():
     """Get password and check length."""
     password = input("Password: ")
     while len(password) < PASSWORD_LENGTH:
         print("Invalid password")
         password = input("Password: ")
-    print('*' * len(password))
+    return password
 
 
 main()
