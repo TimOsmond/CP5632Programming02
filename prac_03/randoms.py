fb9aea9264a505543cab4b62d0228625a8831271
Tim Osmond <timothy.osmond@my.jcu.edu.au>
2022-09-27 12:49:43 +1000

    Practice using random.randint function.

---
+++ prac_03/randoms.py
@@ -0,0 +1,30 @@
+"""
+Test the import of functions and ability for:
+import random
+    print(random.randint(5, 20))  # line 1
+    print(random.randrange(3, 10, 2))  # line 2
+    print(random.uniform(2.5, 5.5))  # line 3
+and
+Write code, not a comment, to produce a random number between 1 and 100 inclusive.
+"""
+
+# What did you see on line 1?
+# 5
+# What was the smallest number you could have seen, what was the largest?
+# smallest 5, largest 20
+
+# What did you see on line 2?
+# 3
+# What was the smallest number you could have seen, what was the largest?
+# smallest 3, largest 9
+# Could line 2 have produced a 4?
+# no
+
+# What did you see on line 3?
+# 3.4934706473081665
+# What was the smallest number you could have seen, what was the largest?
+# smallest 1.0, largest 5.5
+
+from random import randint
+
+print(randint(0, 10))
