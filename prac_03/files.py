78568a65e2f0898fdfee9c1feb369ebb9dabb7d4
Tim Osmond <timothy.osmond@my.jcu.edu.au>
2022-09-28 11:50:36 +1000

    Read file and add numbers from file

--- prac_03/files.py
+++ prac_03/files.py
@@ -5,11 +5,32 @@ OUTPUT_FILE = "name.txt"
 
 
 def main():
-    """Print text to a file."""
+    """Print to and read from a file."""
+    #  Code 1.
     name = input("Name: ")
     out_file = open(OUTPUT_FILE, "w")
     print(name, file=out_file)
     out_file.close()
+    #  Code 2.
+    in_file = open(OUTPUT_FILE, "r")
+    read_line = in_file.readline()
+    in_file.close()
+    print(f"Your name is {read_line}")
+    # Code 3.
+    in_file = open("numbers.txt", "r")
+    number1 = int(in_file.readline())
+    number2 = int(in_file.readline())
+    in_file.close()
+    result = number1 + number2
+    print(result)
+    #  Code 4.
+    in_file = open("numbers.txt", "r")
+    result = 0
+    for line in in_file:
+        number = int(line)
+        result += number
+    in_file.close()
+    print(result)
 
 
 main()
