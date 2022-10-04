2440ca48ac750527bf0f6debdc53c3546cb50501
Tim Osmond <timothy.osmond@my.jcu.edu.au>
2022-10-04 15:29:22 +1000

    Modify select numbers.

--- prac_04/quick_picks.py
+++ prac_04/quick_picks.py
@@ -9,4 +9,12 @@ MINIMUM = 1
 MAXIMUM = 45
 
 number_of_quick_picks = int(input("How many quick picks? "))
-
+for i in range(number_of_quick_picks):
+    quick_pick = []
+    for j in range(NUMBERS_PER_LINE):
+        number = random.randint(MINIMUM, MAXIMUM)
+        while number in quick_pick:
+            number = random.randint(MINIMUM, MAXIMUM)
+        quick_pick.append(number)
+    quick_pick.sort()
+    print(" ".join("{}".format(number) for number in quick_pick))
