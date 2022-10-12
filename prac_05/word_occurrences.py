d8877a024607c68ee5c5ff1395500570f6f621af
Tim Osmond <timothy.osmond@my.jcu.edu.au>
2022-10-13 07:28:14 +1000

    Print number of words in a sentence.

---
+++ prac_05/word_occurrences.py
@@ -0,0 +1,16 @@
+"""
+Word Occurrences
+Count the number of each word in a sentence.
+Estimate: 30 minutes
+Actual:   32 minutes
+"""
+
+words = (input("What is the sentence: ").split(" "))
+print(words)
+word_to_count = {}
+for word in words:
+    word_to_count[word] = word_to_count.get(word, 0) + 1
+max_word_length = max((len(word) for word in word_to_count))
+for word, count in word_to_count.items():
+    print(f"{word:{max_word_length}} : {count}")
+
