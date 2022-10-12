deab3935d5fe2201d62ba9b9433e6fe0f8904685
Tim Osmond <timothy.osmond@my.jcu.edu.au>
2022-10-13 07:47:04 +1000

    Extract name from email address.

--- prac_05/emails.py
+++ prac_05/emails.py
@@ -3,3 +3,12 @@ Extract name from email address and print list.
 Estimate: 40 minutes
 Actual:   32 minutes
 """
+
+email = input("Email: ")
+email_to_name = {}
+while email != "":
+    name = email.split("@")[0].split(".")
+    name = " ".join(name).title()
+    email_to_name[email] = name
+    # email = input("Email: ")
+    print(email_to_name)
