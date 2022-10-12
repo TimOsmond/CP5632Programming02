fd0f9f59b397dc5281b69167c3bd1006aa881f6e
Tim Osmond <timothy.osmond@my.jcu.edu.au>
2022-10-12 19:43:59 +1000

    Convert dictionary names to lowercase for search.

--- prac_05/hex_colours.py
+++ prac_05/hex_colours.py
@@ -7,10 +7,11 @@ COLOUR_TO_HEX = {"AliceBlue": "#f0f8ff", "AntiqueWhite": "#faebd7",
                  "aquamarine1": "#7fffd4", "aquamarine2": "#76eec6",
                  "aquamarine4": "#458b74", "azure1": "#f0ffff"}
 
-colour_name = input("Enter a colour name: ")
+lowercase_colours_to_hex = {key.lower(): value for key, value in COLOUR_TO_HEX.items()}
+colour_name = input("Enter a colour name: ").lower()
 while colour_name != "":
-    if colour_name not in COLOUR_TO_HEX:
-        print("Invalid colour name")
+    if colour_name not in lowercase_colours_to_hex:
+        print("Colour not found")
     else:
-        print(f"{colour_name} is {COLOUR_TO_HEX[colour_name]}")
-    colour_name = input("Enter a colour name: ")
+        print(f"{colour_name} is {lowercase_colours_to_hex[colour_name]}")
+    colour_name = input("Enter a colour name: ").lower()
