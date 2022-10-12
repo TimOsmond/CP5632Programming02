8a0831743b6eb77d063feee512a66aff0965310f
Tim Osmond <timothy.osmond@my.jcu.edu.au>
2022-10-13 07:34:15 +1000

    Print list sorted in alphabetical order and deal with capital letters.

--- prac_05/word_occurrences.py
+++ prac_05/word_occurrences.py
@@ -1,16 +1,15 @@
 """
 Word Occurrences
-Count the number of each word in a sentence.
+Count the number of each word in a sentence and print alphabetically.
 Estimate: 30 minutes
-Actual:   32 minutes
+Actual:   35 minutes
 """
 
-words = (input("What is the sentence: ").split(" "))
-print(words)
+words = (input("What is the sentence: ").lower().split(" "))
+words.sort()
 word_to_count = {}
 for word in words:
     word_to_count[word] = word_to_count.get(word, 0) + 1
 max_word_length = max((len(word) for word in word_to_count))
 for word, count in word_to_count.items():
     print(f"{word:{max_word_length}} : {count}")
-
