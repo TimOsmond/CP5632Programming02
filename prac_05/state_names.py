01c9743a7de16ecb711d2d6abf34392e1dd8608d
Tim Osmond <timothy.osmond@my.jcu.edu.au>
2022-10-12 15:33:16 +1000

    Easier to ask forgiveness than permission error handling.

--- prac_05/state_names.py
+++ prac_05/state_names.py
@@ -13,8 +13,8 @@ for state in CODE_TO_NAME:
     print(f"{state:3} is {CODE_TO_NAME[state]}")
 state_code = input("Enter short state: ").upper()
 while state_code != "":
-    if state_code in CODE_TO_NAME:
+    try:
         print(f"{state_code} is {CODE_TO_NAME[state_code]}")
-    else:
+    except KeyError:
         print("Invalid short state")
     state_code = input("Enter short state: ").upper()
