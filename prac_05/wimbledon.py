e414b7c421a85446653aee15878d81bae01fba13
Tim Osmond <timothy.osmond@my.jcu.edu.au>
2022-10-13 12:28:10 +1000

    Refactor and print results.

--- prac_05/wimbledon.py
+++ prac_05/wimbledon.py
@@ -10,20 +10,8 @@ FILENAME = "wimbledon.csv"
 def main():
     """Read Wimbledon csv results and print champions and countries."""
     winners_details = read_results_file(FILENAME)
-    # print(winners_details)
-    winners_count = {}
-    winners_countries = set()
-    for countries in winners_details:
-        winners_countries.add(countries[1])
-    for winner in winners_details:
-        try:
-            winners_count[winner[2]] += 1
-        except KeyError:
-            winners_count[winner[2]] = 1
-
-    print(f"These {len(winners_countries)} countries have won Wimbledon")
-    print(", ".join(sorted(winners_countries)))
-    print(winners_count)
+    winners_count, winners_countries = get_results(winners_details)
+    print_results(winners_count, winners_countries)
 
 
 def read_results_file(filename):
@@ -35,6 +23,29 @@ def read_results_file(filename):
             bits = line.strip().split(",")
             winners_details.append(bits)
     return winners_details
+
+
+def get_results(winners_details):
+    """ Get list results and return winners and countries."""
+    winners_count = {}
+    for winner in winners_details:
+        try:
+            winners_count[winner[2]] += 1
+        except KeyError:
+            winners_count[winner[2]] = 1
+    winners_countries = set()
+    for countries in winners_details:
+        winners_countries.add(countries[1])
+    return winners_count, winners_countries
+
+
+def print_results(winners_count, winners_countries):
+    """Print the results."""
+    print("Wimbledon Champions:")
+    for winner, count in winners_count.items():
+        print(f"{winner} {count}")
+    print(f"\nThese {len(winners_countries)} countries have won Wimbledon")
+    print(", ".join(sorted(winners_countries)))
 
 
 main()
