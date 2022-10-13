b14bfc040861c3e4fe8d1d805e2ae5864a4e9508
Tim Osmond <timothy.osmond@my.jcu.edu.au>
2022-10-13 11:18:55 +1000

    Extract winner names to a set.

--- prac_05/wimbledon.py
+++ prac_05/wimbledon.py
@@ -4,20 +4,29 @@ Estimate: 60 minutes
 Actual:   32 minutes
 """
 
+FILENAME = "wimbledon.csv"
+
 
 def main():
-    filename = "wimbledon.csv"
-    read_results_file(filename)
+    """Read Wimbledon csv results and print champions and countries."""
+    winners_details = read_results_file(FILENAME)
+    print(winners_details)
+    winners_count = {}
+    winner_names = set()
+    for winner in winners_details:
+        winner_names.add(winner[2])
+    print(winner_names)
 
 
 def read_results_file(filename):
-    winner_details = []
+    """Read the csv results file and create list."""
+    winners_details = []
     with open(filename, "r", encoding="utf-8-sig") as in_file:
         in_file.readline()
         for line in in_file:
             bits = line.strip().split(",")
-            winner_details.append(bits)
-        print(winner_details)
+            winners_details.append(bits)
+    return winners_details
 
 
 main()
