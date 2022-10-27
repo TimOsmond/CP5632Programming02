67ed7e623c9be29ae502a4083c6799bf6bc30f45
Tim Osmond <99766621+TimOsmond@users.noreply.github.com>
2022-10-28 09:19:19 +1000

    Fix exception errors for cost and year.

--- prac_06/guitars.py
+++ prac_06/guitars.py
@@ -1,5 +1,5 @@
 """
-Store a list of guitarsand print details.
+Store a list of guitars and print details.
 """
 from prac_06.guitar import Guitar
 
@@ -11,30 +11,38 @@ def main():
     print("My guitars!")
     name = input("Name: ")
 
-    guitars.append(Guitar("Gibson L-5 CES", 1922, 16035.40))
-    guitars.append(Guitar("Line 6 JTV-59", 2010, 1512.9))
+    # guitars.append(Guitar("Gibson L-5 CES", 1922, 16035.40))
+    # guitars.append(Guitar("Line 6 JTV-59", 2010, 1512.9))
 
     while name != "":
-        # name = input("Name: ")
-        year = int(input("Year: "))
-        cost = input("Cost: ")
-        if "$" in cost:
+        is_valid_input = False
+        while not is_valid_input:
             try:
-                cost = float(cost[1:])
+                year = int(input("Year: "))
+                is_valid_input = True
+            except ValueError:
+                print("Invalid year")
+
+        is_valid_input = False
+        while not is_valid_input:
+            try:
+                cost = input("Cost: ")
+                if "$" in cost:
+                    cost = float(cost[1:])
+                cost = float(cost)
+                is_valid_input = True
             except ValueError:
                 print("Invalid cost")
-                cost = input("Cost: ")
-        cost = float(cost)
         guitars.append(Guitar(name, year, cost))
         print(f"{name} ({year}) : ${cost:.2f} added.")
         name = input("\nName: ")
-
     print("These are my guitars:")
     for i, guitar in enumerate(guitars):
         vintage_string = ""
         if guitar.is_vintage():
             vintage_string = "(vintage)"
-        print(f"Guitar {i + 1}: {guitar.name:>20} ({guitar.year}), worth ${guitar.cost:10,.2f} {vintage_string}")
+        print(f"Guitar {i + 1}: {guitar.name:>20} ({guitar.year}), "
+              f"worth ${guitar.cost:10,.2f} {vintage_string}")
 
 
 main()
