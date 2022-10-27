1e4d3a888971560609c0dc5314a02c962bb83266
Tim Osmond <99766621+TimOsmond@users.noreply.github.com>
2022-10-27 15:02:05 +1000

    Read lists and return languages that are dynamic.

--- prac_06/programming_language.py
+++ prac_06/programming_language.py
@@ -1,18 +1,22 @@
 """
-
+Analyse programming languages from given lists.
 """
 
 
 class ProgrammingLanguage:
+    """Language details from lists"""
+
     def __init__(self, name="", typing="", reflection="", year=""):
         self.name = name
         self.typing = typing
         self.reflection = reflection
         self.year = year
 
-    def is_dynamic(self):
-        return self.typing == "Dynamic"
-
     def __str__(self):
-        return "{}, {} Typing, Reflection={}, First appeared in {}".format(self.name, self.typing, self.reflection,
-                                                                           self.year)
+        """Return string of language details from lists"""
+        return f"{self.name}, {self.typing}, Reflection={self.reflection}, " \
+               f"First appeared in {self.year}"
+
+    def is_dynamic(self):
+        """Check if is dynamic"""
+        return self.typing == "Dynamic"
