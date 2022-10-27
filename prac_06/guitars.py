521ee32db68fb7b707d44352390cb00c171dd88b
Tim Osmond <99766621+TimOsmond@users.noreply.github.com>
2022-10-28 08:52:38 +1000

    Add try except for cost and remove $ from price input.

--- prac_06/guitars.py
+++ prac_06/guitars.py
@@ -8,24 +8,33 @@ def main():
     """Store a list of guitars and print their details"""
 
     guitars = []
-    # print("My guitars!")
+    print("My guitars!")
+    name = input("Name: ")
 
     guitars.append(Guitar("Gibson L-5 CES", 1922, 16035.40))
     guitars.append(Guitar("Line 6 JTV-59", 2010, 1512.9))
 
-    while guitar != "":
-        guitar = input("Name: ")
+    while name != "":
+        # name = input("Name: ")
         year = int(input("Year: "))
-        cost = float(input("Cost: "))
-        guitars.append(Guitar(guitar, year, cost))
+        cost = input("Cost: ")
+        if "$" in cost:
+            try:
+                cost = float(cost[1:])
+            except ValueError:
+                print("Invalid cost")
+                cost = input("Cost: ")
+        cost = float(cost)
+        guitars.append(Guitar(name, year, cost))
+        print(f"{name} ({year}) : ${cost:.2f} added.")
+        name = input("\nName: ")
 
     print("These are my guitars:")
     for i, guitar in enumerate(guitars):
         vintage_string = ""
         if guitar.is_vintage():
-            vintage_string = " (vintage)"
+            vintage_string = "(vintage)"
         print(f"Guitar {i + 1}: {guitar.name:>20} ({guitar.year}), worth ${guitar.cost:10,.2f} {vintage_string}")
-        print(f"Guitar {i}: {guitar.name:>20} ({guitar.year}), worth ${guitar.cost:10,.2f}{vintage_string}")
 
 
 main()
