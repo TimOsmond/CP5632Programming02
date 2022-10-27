221980f1884bc61c4c10d690ac4f903728e6edf5
Tim Osmond <99766621+TimOsmond@users.noreply.github.com>
2022-10-28 08:26:47 +1000

    Initial commit.

--- prac_06/guitars.py
+++ prac_06/guitars.py
@@ -1,25 +1,31 @@
 """
-Guitars class
-Estimated 60
-1510
+Store a list of guitarsand print details.
 """
+from prac_06.guitar import Guitar
 
-import datetime
+
+def main():
+    """Store a list of guitars and print their details"""
+
+    guitars = []
+    # print("My guitars!")
+
+    guitars.append(Guitar("Gibson L-5 CES", 1922, 16035.40))
+    guitars.append(Guitar("Line 6 JTV-59", 2010, 1512.9))
+
+    while guitar != "":
+        guitar = input("Name: ")
+        year = int(input("Year: "))
+        cost = float(input("Cost: "))
+        guitars.append(Guitar(guitar, year, cost))
+
+    print("These are my guitars:")
+    for i, guitar in enumerate(guitars):
+        vintage_string = ""
+        if guitar.is_vintage():
+            vintage_string = " (vintage)"
+        print(f"Guitar {i + 1}: {guitar.name:>20} ({guitar.year}), worth ${guitar.cost:10,.2f} {vintage_string}")
+        print(f"Guitar {i}: {guitar.name:>20} ({guitar.year}), worth ${guitar.cost:10,.2f}{vintage_string}")
 
 
-class Guitar:
-    """Get a Guitar object."""
-
-    def __init__(self, name="", year=0, cost=0):
-        self.name = name
-        self.year = year
-        self.cost = cost
-
-    def __str__(self):
-        """Return a string representation of Guitar object."""
-        return f"{self.name} ({self.year}) : ${self.cost:.2f}"
-
-    def get_age(self):
-        """Get the age of the guitar."""
-        year = datetime.date.today()
-        return year - self.year
+main()
