04021bee47cf70d51d5dfee5133e74b3ca668af4
Tim Osmond <99766621+TimOsmond@users.noreply.github.com>
2022-11-08 19:27:47 +1000

    Read guitar list from file and print.

--- prac_07/guitar.py
+++ prac_07/guitar.py
@@ -4,8 +4,6 @@ Estimated 60
 1510
 """
 
-import datetime
-
 
 class Guitar:
     """Get a Guitar object."""
@@ -17,15 +15,4 @@ class Guitar:
 
     def __str__(self):
         """Return a string representation of Guitar object."""
-        return f"{self.name} ({self.year}) : ${self.cost:.2f}"
-
-    def get_age(self):
-        """Get the age of the guitar."""
-        today = datetime.date.today()
-        current_year = today.year
-        # print(current_year)
-        return current_year - self.year
-
-    def is_vintage(self):
-        """Check if the guitar is vintage."""
-        return self.get_age() >= 50
+        return f"{self.name:>25} ({self.year}) : ${self.cost:10,.2f}"
